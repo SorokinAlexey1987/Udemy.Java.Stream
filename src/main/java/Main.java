667890b00e3132bef85015460c642a3d9fdb3989
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-29 19:27:36 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        /*
         //new Thread(() -> System.out.println(1)).start();
         Director director = new Director();
         Worker worker = (n) -> {
@@ -10,5 +14,25 @@ public class Main {
         };
         String result = director.force(worker, 5);
         System.out.println(result);
+        */
+
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            numbers.add((int) (Math.random() * 1000));
+        }
+        List<Integer> filtered = filter(numbers);
+        for (int i : filtered) {
+            System.out.println(i);
+        }
+    }
+
+    private static List<Integer> filter(List<Integer> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int i : list) {
+            if (i % 2 == 0) {
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
